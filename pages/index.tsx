9d35f43df7c3e8428d000090f5f35e79182d8d4e
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-07-03 13:46:39 +0530

    login and logout sorted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+	const { data: session } = useSession();
+	if (!session) {
+		return (
+			<div className="bg-blue-900 w-screen h-screen flex items-center">
+				<div className="text-center w-full text-white">
+					<button
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 px-4 rounded-lg"
+						onClick={() => signIn("google")}>
+						{" "}
+						Login with Google
+					</button>
+				</div>
+			</div>
+		);
+	}
+
 	return (
-		<div className="bg-blue-800 w-screen h-screen flex items-center">
+		<div className="bg-blue-900 w-screen h-screen flex items-center">
 			<div className="text-center w-full text-white">
-				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 px-4 rounded-lg">
+				<h1 className="text-5xl font-bold mb-4">Welcome {session.user.name}</h1>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 px-4 rounded-lg"
+					onClick={() => signOut()}>
 					{" "}
-					Login with Google
+					Logout
 				</button>
 			</div>
 		</div>
