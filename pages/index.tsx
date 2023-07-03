6d172ce27cc186371cad6f69e4d3a84e5392f000
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-07-03 13:31:12 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<div className="bg-blue-900 w-screen h-screen flex items-center">
+			<div className="text-center w-full text-white">
+				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 px-4 rounded-lg">
+					{" "}
+					Login with Google
+				</button>
+			</div>
+		</div>
+	);
+}
