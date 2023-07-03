367db1a4235f3c9b7deb1218c8f77b303b2b15c6
akashdeeep <akashdeep@iitbhilai.ac.in>
2023-07-03 13:38:35 +0530

    temp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<div className="bg-blue-900 w-screen h-screen flex items-center">
+		<div className="bg-blue-800 w-screen h-screen flex items-center">
 			<div className="text-center w-full text-white">
 				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 px-4 rounded-lg">
 					{" "}
